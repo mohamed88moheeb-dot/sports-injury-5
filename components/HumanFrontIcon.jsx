af6a317f1bacc6cff536852f72fcac221bac8fc4
mohamed88moheeb-dot <mohamed88moheeb@gmail.com>
2026-06-09 22:55:47 +0300

    Create HumanFrontIcon.jsx

---
+++ components/HumanFrontIcon.jsx
@@ -0,0 +1,9 @@
+export default function HumanFrontIcon({ className = '' }) {
+  return (
+    <img
+      src="/human_front.svg"
+      alt="Human body front view"
+      className={className}
+    />
+  );
+}
